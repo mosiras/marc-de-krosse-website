ab04a5bf332462e7f1a5bd6758585acd7b21ba37
1306504 <alessandro.gubler@gmail.com>
2020-10-20 09:32:38 +0200

    contact yes yes

---
+++ Scripts/HTML/contact.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Website: \n Message: $message";
+$recipient = "m.dekrosse@o2g2.nl";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
